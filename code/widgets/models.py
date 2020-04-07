4ed64be0ceaee828fb5a50578319420d07b4056b
Paul Savala <paulsavala@gmail.com>
2020-04-07 11:06:32 -0500

    Widget update

--- code/widgets/models.py
+++ code/widgets/models.py
@@ -34,23 +34,29 @@ class GenericModelWidget:
 
 
 class SeirModelWidget(GenericModelWidget):
+    def _fix_marks(self, marks):
+        # Dash bug causes certain marks to not show up.
+        # Solution taken from https://github.com/plotly/dash-core-components/issues/159
+        marks = {int(i) if i % 1 == 0 else i: '{}'.format(np.round(i, 3)) for i in marks}
+        return marks
+
     def _slider(self, param, steps):
         if param.is_int:
             steps = int(np.floor(steps))
-            marks_range = range(param.min_value, param.max_value+1, steps)
+            marks_range = range(param.min_value, param.max_value+steps, steps)
         else:
-            marks_range = np.arange(param.min_value, param.max_value+1, steps)
-            
+            marks_range = np.arange(param.min_value, param.max_value+steps, steps)
+
         slider = dcc.Slider(id=f'{self.name}-{param.name}-slider',
                             min=param.min_value,
                             max=param.max_value,
                             step=steps,
                             value=param.default_value,
-                            marks={s: str(s) for s in marks_range}
+                            marks=self._fix_marks(marks_range)
                             )
         return slider
 
-    def _sliders(self):
+    def sliders(self):
         param_groups = defaultdict(list)
         for param in self.model.params:
             param_groups[param.group].append(param)
@@ -60,44 +66,16 @@ class SeirModelWidget(GenericModelWidget):
             pass
 
         slider_children = defaultdict(list)
-        # Advanced sliders
-        group = 'advanced'
-        for param in param_groups.get(group):
-            steps = (param.max_value - param.min_value) / 10
-            slider_children[group].append(
-                html.Label(f'{param.desc} ({param.name}):')
-            )
-            slider = self._slider(param, steps)
-
-
-
-    def sliders(self):
-        slider_children = []
-        for param in self.model.params:
-            if not param.is_constant:
+        groups = ['advanced', 'social_distancing']
+        for group in groups:
+            for param in param_groups.get(group):
                 steps = (param.max_value - param.min_value) / 10
-                slider_children.append(
-                    html.Label(f'{param.desc} ({param.name}):')
-                )
-                if param.is_int:
-                    slider_children.append(
-                        dcc.Slider(id=f'{self.name}-{param.name}-slider', min=param.min_value, max=param.max_value,
-                                   step=int(np.floor(steps)), value=param.default_value,
-                                   marks={np.floor(s): str(np.floor(s)) for s in range(param.min_value,
-                                                                                       param.max_value,
-                                                                                       int(np.floor(steps))
-                                                                                       )
-                                          }
-                                   )
-                    )
-                else:
-                    slider_children.append(
-                        dcc.Slider(id=f'{self.name}-{param.name}-slider', min=param.min_value, max=param.max_value,
-                                   step=steps, value=param.default_value,
-                                   marks={s: str(np.round(s, 3)) for s in
-                                          np.arange(param.min_value, param.max_value, steps)})
-                    )
-        slider_children.append(
+                slider_children[group].append(html.Div(children=[
+                    html.Label(f'{param.desc} ({param.name}):'),
+                    self._slider(param, steps)
+                ]))
+
+        slider_children['checkboxes'].append(
             dcc.Checklist(id=f'{self.name}-prevalence-checkbox',
                           options=[
                               {'label': 'Prevalence', 'value': 'prevalence'}
@@ -105,9 +83,52 @@ class SeirModelWidget(GenericModelWidget):
                           value=[]
                           )
         )
+        slider_children_groups = [html.Div(id=f'{self.name}-{group}-sliders',
+                                  children=slider_children.get(group))
+                                  for group in groups]
+        slider_children_groups += [html.Div(id=f'{self.name}-checkboxes-sliders',
+                                            children=slider_children.get('checkboxes'))]
 
-        sliders = html.Div(id=f'{self.name}-sliders', children=slider_children)
+        sliders = html.Div(id=f'{self.name}-sliders', children=slider_children_groups)
         return sliders
+
+    # def sliders(self):
+    #     slider_children = []
+    #     for param in self.model.params:
+    #         if not param.is_constant:
+    #             steps = (param.max_value - param.min_value) / 10
+    #             slider_children.append(
+    #                 html.Label(f'{param.desc} ({param.name}):')
+    #             )
+    #             if param.is_int:
+    #                 slider_children.append(
+    #                     dcc.Slider(id=f'{self.name}-{param.name}-slider', min=param.min_value, max=param.max_value,
+    #                                step=int(np.floor(steps)), value=param.default_value,
+    #                                marks={np.floor(s): str(np.floor(s)) for s in range(param.min_value,
+    #                                                                                    param.max_value,
+    #                                                                                    int(np.floor(steps))
+    #                                                                                    )
+    #                                       }
+    #                                )
+    #                 )
+    #             else:
+    #                 slider_children.append(
+    #                     dcc.Slider(id=f'{self.name}-{param.name}-slider', min=param.min_value, max=param.max_value,
+    #                                step=steps, value=param.default_value,
+    #                                marks={s: str(np.round(s, 3)) for s in
+    #                                       np.arange(param.min_value, param.max_value, steps)})
+    #                 )
+    #     slider_children.append(
+    #         dcc.Checklist(id=f'{self.name}-prevalence-checkbox',
+    #                       options=[
+    #                           {'label': 'Prevalence', 'value': 'prevalence'}
+    #                       ],
+    #                       value=[]
+    #                       )
+    #     )
+    #
+    #     sliders = html.Div(id=f'{self.name}-sliders', children=slider_children)
+    #     return sliders
 
     def header(self):
         return html.H1(children='Covid-19 SEIR Model [K-T-L-G]')
